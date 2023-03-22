eeec532f01b6fc5842383b1349e66c5bfee74845
isaac640 <isaac.cabriadiez@iesmiguelherrero.com>
2023-03-22 15:30:50 +0100

    borrado el ejercicio 3, ejercicio incorrecto

--- UT2/Ejercicio_3_Parte_2/src/main/kotlin/Main.kt
+++
@@ -1,8 +0,0 @@
-fun main() {
-   mediaNota("DAW2",6.1f,9.8f,10f, tutor="Carmen")
-   mediaNota("DAM2", 5.1f,4.6f,2.8f)
-}
-
-fun mediaNota(grupo: String, vararg notas: Float, tutor: String="Iván") {
-   println("Media de notas del grupo $grupo: ${"%.2f".format(notas.average())} Tutor: $tutor")
-}
